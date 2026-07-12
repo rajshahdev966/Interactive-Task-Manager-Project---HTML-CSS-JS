8baf4b30979dda907c27fc7244220c42c6034ab3
rajshahdev966 <rajshahdev966@users.noreply.github.com>
2026-07-12 11:33:17 +0530

    Modified website assets

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
+
+
+
+const name = document.querySelector("#user-name")
+const loginBtn = document.querySelector("#login-button")
+const userName = name.value;
 const navLinks = document.querySelectorAll(".nav-buttons")
 const homeButton = navLinks[0]
 const planButton = navLinks[1]
 const todoButton = navLinks[2]
 const settingButton = navLinks[3]
 
+loginBtn.addEventListener('click', ()=>{
+    
+})
 
 
 
