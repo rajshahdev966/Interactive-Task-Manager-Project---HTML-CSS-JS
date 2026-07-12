3d1e05d1449e950b96d79d92d7da81a18e840741
rajshahdev966 <rajshahdev966@users.noreply.github.com>
2026-07-12 11:17:50 +0530

    Modified website assets

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-const homeButton = document.querySelector(".nav-buttons")
-console.log(homeButton);
-console.log("Hello");
+const navLinks = document.querySelectorAll(".nav-buttons")
+const homeButton = navLinks[0]
+const planButton = navLinks[1]
+const todoButton = navLinks[2]
+const settingButton = navLinks[3]
+
+
 
 
 
