d45980d0bc6cd36444bc453c0c3fdba49d58f15b
rajshahdev966 <rajshahdev966@users.noreply.github.com>
2026-07-12 20:30:40 +0530

    Auto sync changes

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const loginForm = document.querySelector('form')
 const loginSection = document.querySelector('.name-form')
 const name = document.querySelector("#user-name")
 const loginBtn = document.querySelector("#login-button")
-let userName;
+let userName = localStorage.getItem('userName');
 const navLinks = document.querySelectorAll(".nav-buttons")
 const homeButton = navLinks[0]
 const planButton = navLinks[1]
@@ -13,10 +13,14 @@ const settingButton = navLinks[3]
     
 loginForm.addEventListener('submit', (events)=>{
     events.preventDefault();
+
+    if(userName){
     userName = name.value;
     console.log(userName)
     loginSection.style.display = "none"
     localStorage.setItem('userName', userName)
+    }
+    
 })
 
 
