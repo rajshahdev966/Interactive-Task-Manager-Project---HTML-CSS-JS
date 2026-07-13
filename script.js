894825392fb36506a25e6e3a124c81964564a19b
rajshahdev966 <rajshahdev966@users.noreply.github.com>
2026-07-13 14:15:52 +0530

    Modified website assets

--- script.js
+++ script.js
@@ -1,285 +1,273 @@
+// LOGIN BAR CONSTANT AND VARIABLE DECLARATION
 
+const loginForm = document.querySelector("form");
+const loginSection = document.querySelector(".name-form");
+const name = document.querySelector("#user-name");
+const loginBtn = document.querySelector("#login-button");
+const profileFirstName = document.querySelector("#profile-first-name");
+let userName = localStorage.getItem("userName");
 
-// LOGIN BAR CONSTANT AND VARIABLE DECLARATION 
+// NAVIGATION BAR CONSTANT AND VARIABLE DECLARATION
+const navLinks = document.querySelectorAll(".nav-buttons");
+const homeButton = navLinks[0];
+const planButton = navLinks[1];
+const todoButton = navLinks[2];
+const settingButton = navLinks[3];
+const profileInitial = document.querySelector(".profile-initial");
+const profileName = document.querySelector("#profile-name");
 
-const loginForm = document.querySelector('form')
-const loginSection = document.querySelector('.name-form')
-const name = document.querySelector("#user-name")
-const loginBtn = document.querySelector("#login-button")
-const profileFirstName = document.querySelector("#profile-first-name")
-let userName = localStorage.getItem('userName');
-
-
-// NAVIGATION BAR CONSTANT AND VARIABLE DECLARATION 
-const navLinks = document.querySelectorAll(".nav-buttons")
-const homeButton = navLinks[0]
-const planButton = navLinks[1]
-const todoButton = navLinks[2]
-const settingButton = navLinks[3]
-const profileInitial = document.querySelector(".profile-initial")
-const profileName = document.querySelector("#profile-name")
-
-// TIME AND DATE SECTION CONSTANT AND VARIABLE DECLARATION 
-const greetTime = document.querySelector("#time-greet")
-const dayUI = document.querySelector("#day")
-const dateUI = document.querySelector("#date")
-const timeUI = document.querySelector("#time")
-const amPm = document.querySelector("#am-pm")
+// TIME AND DATE SECTION CONSTANT AND VARIABLE DECLARATION
+const greetTime = document.querySelector("#time-greet");
+const dayUI = document.querySelector("#day");
+const dateUI = document.querySelector("#date");
+const timeUI = document.querySelector("#time");
+const amPm = document.querySelector("#am-pm");
 
 // TODO LIST CONSTANT AND VARIABLE DECLARATION
 
-const taskAddForm = document.querySelector("#todo-task-add-form")
-const newTaskInput = document.querySelector("#task-input")
-const taskAddButton = document.querySelector("#task-add-button")
-const taskCollection = document.querySelector(".todo-task-collection")
-const deleteButton = document.querySelector(".ri-delete-bin-line")
-const checkBox = document.querySelector("#checkbox")
-const completedTaskCount = document.querySelector("#completed-todo-tasks")
-const totalTaskCount = document.querySelector("#total-todo-tasks")
-const totalTimeline = document.querySelector(".todo-total-timeline")
-const doneTimeline = document.querySelector(".todo-done-timeline")
+const taskAddForm = document.querySelector("#todo-task-add-form");
+const newTaskInput = document.querySelector("#task-input");
+const taskAddButton = document.querySelector("#task-add-button");
+const taskCollection = document.querySelector(".todo-task-collection");
+const deleteButton = document.querySelector(".ri-delete-bin-line");
+const checkBox = document.querySelector("#checkbox");
+const completedTaskCount = document.querySelector("#completed-todo-tasks");
+const totalTaskCount = document.querySelector("#total-todo-tasks");
+const totalTimeline = document.querySelector(".todo-total-timeline");
+const doneTimeline = document.querySelector(".todo-done-timeline");
 
+let taskCollectionArr = JSON.parse(localStorage.getItem("allTasks")) || [];
 
-
-let taskCollectionArr = JSON.parse(localStorage.getItem('allTasks')) || [];
-
-let deleteTask; 
+let deleteTask;
 let taskCheck;
-let allCheckBox; 
-let allTaskText
-let statusUpdate
+let allCheckBox;
+let allTaskText;
+let statusUpdate;
 
-let taskUpdateUI = ()=>{
-    taskCollection.innerHTML = ""
-    taskCollectionArr.forEach((elem, index)=>{
-        taskCollection.innerHTML += `
+let taskUpdateUI = () => {
+  taskCollection.innerHTML = "";
+  taskCollectionArr.forEach((elem, index) => {
+    taskCollection.innerHTML += `
         <div class="todo-task">
                   <div>
-                      <i class="${elem.completed ? 'ri-checkbox-circle-line': 'ri-checkbox-blank-circle-line' }" id="checkbox" onclick="taskCheck(${index})"></i>
+                      <i class="${elem.completed ? "ri-checkbox-circle-line" : "ri-checkbox-blank-circle-line"}" id="checkbox" onclick="taskCheck(${index})"></i>
                       <!-- Make it checkbox-circle-line to make it filled -->
-                      <span id="task-text" style="text-decoration: ${elem.completed ? 'line-through' : 'none'}">${elem.text}</span>
+                      <span id="task-text" style="text-decoration: ${elem.completed ? "line-through" : "none"}">${elem.text}</span>
                   </div>
                   <i class="ri-delete-bin-line" onclick="deleteTask(${index})"></i>
-                </div>`
-        
-    })
+                </div>`;
+  });
 
-    totalTaskCount.innerHTML = taskCollectionArr.length
-    allCheckBox = document.querySelectorAll("#checkbox")
-    allTaskText = document.querySelectorAll("#task-text")
-
-}
+  totalTaskCount.innerHTML = taskCollectionArr.length;
+  allCheckBox = document.querySelectorAll("#checkbox");
+  allTaskText = document.querySelectorAll("#task-text");
+};
 taskUpdateUI();
 
+taskAddForm.addEventListener("submit", (events) => {
+  events.preventDefault();
+  if (newTaskInput.value.trim() == "") return;
+  taskCollectionArr.push({
+    text: newTaskInput.value,
+    completed: false,
+  });
+  localStorage.setItem("allTasks", JSON.stringify(taskCollectionArr));
+  taskUpdateUI();
+  taskAddForm.reset();
+});
 
-taskAddForm.addEventListener('submit', (events)=>{
-    events.preventDefault();
-    if(newTaskInput.value.trim() == "") return;
-    taskCollectionArr.push(
-        {
-            text: newTaskInput.value,
-            completed: false,
-        });
-    localStorage.setItem('allTasks', JSON.stringify(taskCollectionArr))
-    taskUpdateUI();
-    taskAddForm.reset();
-})
+deleteTask = (index) => {
+  taskCollectionArr.splice(index, 1);
+  localStorage.setItem("allTasks", JSON.stringify(taskCollectionArr));
+  taskUpdateUI();
+};
+allCheckBox = document.querySelectorAll("#checkbox");
+allTaskText = document.querySelectorAll("#task-text");
 
-deleteTask = (index)=>{
-    taskCollectionArr.splice(index, 1);
-    localStorage.setItem('allTasks', JSON.stringify(taskCollectionArr))
-    taskUpdateUI();
-}
-allCheckBox = document.querySelectorAll("#checkbox")
-allTaskText = document.querySelectorAll("#task-text")
+let allTickCheckBox;
+completedTaskCount.innerHTML = 0;
 
-let allTickCheckBox ;
-completedTaskCount.innerHTML = 0
-
-let checkUpdate = ()=>{
-    allTickCheckBox = document.querySelectorAll(".ri-checkbox-circle-line")
-    completedTaskCount.innerHTML = allTickCheckBox.length
-    doneTimeline.style.width = `${((allTickCheckBox.length / taskCollectionArr.length)*100)}%`
-}
+let checkUpdate = () => {
+  allTickCheckBox = document.querySelectorAll(".ri-checkbox-circle-line");
+  completedTaskCount.innerHTML = allTickCheckBox.length;
+  doneTimeline.style.width = `${(allTickCheckBox.length / taskCollectionArr.length) * 100}%`;
+};
 checkUpdate();
 
-taskCheck = (index)=>{    
-    taskCollectionArr[index].completed = !taskCollectionArr[index].completed
-    localStorage.setItem('allTasks', JSON.stringify(taskCollectionArr))
-    taskUpdateUI();
-    checkUpdate();
+taskCheck = (index) => {
+  taskCollectionArr[index].completed = !taskCollectionArr[index].completed;
+  localStorage.setItem("allTasks", JSON.stringify(taskCollectionArr));
+  taskUpdateUI();
+  checkUpdate();
+};
+
+// LOGIN SECTION LOGIC CODE
+
+let profileUpdate = () => {
+  profileInitial.innerHTML = `<span>${userName[0]}</span>`;
+  profileName.innerHTML = `${userName}`;
+  profileFirstName.innerHTML = `${userName.split(" ")[0]}`;
+};
+
+if (userName) {
+  loginSection.style.display = "none";
+  profileUpdate();
 }
 
+loginForm.addEventListener("submit", (events) => {
+  events.preventDefault();
+  userName = name.value;
+  if (userName.trim() === "") return;
+  console.log(userName);
+  loginSection.style.display = "none";
+  localStorage.setItem("userName", userName);
+  profileUpdate();
+});
 
+// TIME AND DATE SECTION LOGIC
 
+let updateTimeAndDate = () => {
+  const now = new Date();
+  const timeInfo = now.toLocaleTimeString("en-US", {
+    timeZone: "Asia/Kolkata",
+    hour: "2-digit",
+    minute: "2-digit",
+    hour12: true,
+  });
+  const dateInfo = now.toDateString("en-US", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
 
+  const dayInfoArr = dateInfo.split(" ");
+  const timeInfoArr = timeInfo.split(" ");
 
-
-
-// LOGIN SECTION LOGIC CODE 
-
-
-let profileUpdate = ()=>{
-    profileInitial.innerHTML = `<span>${userName[0]}</span>`
-    profileName.innerHTML = `${userName}`
-    profileFirstName.innerHTML = `${userName.split(" ")[0]}`
-    
-}
-
-if(userName){
-    loginSection.style.display = "none"
-    profileUpdate();
-}
-
-loginForm.addEventListener('submit', (events)=>{
-    events.preventDefault();
-    userName = name.value;
-    if(userName.trim() === "") return;
-    console.log(userName)
-    loginSection.style.display = "none"
-    localStorage.setItem('userName', userName)
-    profileUpdate();
-    
-})
-
-// TIME AND DATE SECTION LOGIC 
-
-let updateTimeAndDate = ()=>{
-    const now = new Date();
-    const timeInfo = now.toLocaleTimeString('en-US', {
-        timeZone: 'Asia/Kolkata',
-        hour: '2-digit',
-        minute:  '2-digit',
-        hour12: true,
-    })
-    const dateInfo = now.toDateString('en-US', {
-        year: 'numeric',
-        month: 'long',
-        day: 'numeric',
-    })
-
-    const dayInfoArr = dateInfo.split(" ")
-    const timeInfoArr = timeInfo.split(" ")
-
-    dayUI.innerHTML = `${dayInfoArr[0]}day`
-    dateUI.innerHTML = `${dayInfoArr[2]} ${dayInfoArr[1]} ${dayInfoArr[3]}`
-    timeUI.innerHTML = `${timeInfoArr[0]}`
-    amPm.innerHTML = ` ${timeInfoArr[1]}`
-
-
-}
+  dayUI.innerHTML = `${dayInfoArr[0]}day`;
+  dateUI.innerHTML = `${dayInfoArr[2]} ${dayInfoArr[1]} ${dayInfoArr[3]}`;
+  timeUI.innerHTML = `${timeInfoArr[0]}`;
+  amPm.innerHTML = ` ${timeInfoArr[1]}`;
+};
 updateTimeAndDate();
-setInterval(updateTimeAndDate, 1000)
-
-
+setInterval(updateTimeAndDate, 1000);
 
 // MOTIVATIONAL QUOTE CONSTANT DECLARATION
 
 const allMotQuote = [
   {
-    "quote": "Discipline is choosing between what you are now and what you want most.",
-    "author": "Abraham Lincoln"
+    quote:
+      "Discipline is choosing between what you are now and what you want most.",
+    author: "Abraham Lincoln",
   },
   {
-    "quote": "Success grows quietly from the small promises you keep to yourself each day.",
-    "author": "Robin Sharma"
+    quote:
+      "Success grows quietly from the small promises you keep to yourself each day.",
+    author: "Robin Sharma",
   },
   {
-    "quote": "Your future is shaped more by your habits than by your hopes or dreams.",
-    "author": "James Clear"
+    quote:
+      "Your future is shaped more by your habits than by your hopes or dreams.",
+    author: "James Clear",
   },
   {
-    "quote": "The hardest step toward success is often the decision to begin today.",
-    "author": "John C. Maxwell"
+    quote:
+      "The hardest step toward success is often the decision to begin today.",
+    author: "John C. Maxwell",
   },
   {
-    "quote": "Great achievements are built from ordinary actions repeated with extraordinary consistency.",
-    "author": "Darren Hardy"
+    quote:
+      "Great achievements are built from ordinary actions repeated with extraordinary consistency.",
+    author: "Darren Hardy",
   },
   {
-    "quote": "You become stronger every time you choose progress over comfort and excuses.",
-    "author": "Mel Robbins"
+    quote:
+      "You become stronger every time you choose progress over comfort and excuses.",
+    author: "Mel Robbins",
   },
   {
-    "quote": "Dreams remain wishes until daily effort transforms them into lasting reality.",
-    "author": "Les Brown"
+    quote:
+      "Dreams remain wishes until daily effort transforms them into lasting reality.",
+    author: "Les Brown",
   },
   {
-    "quote": "Every meaningful victory begins with a mindset that refuses to surrender easily.",
-    "author": "Tony Robbins"
+    quote:
+      "Every meaningful victory begins with a mindset that refuses to surrender easily.",
+    author: "Tony Robbins",
   },
   {
-    "quote": "Confidence is earned by keeping the commitments you make to yourself consistently.",
-    "author": "Brian Tracy"
+    quote:
+      "Confidence is earned by keeping the commitments you make to yourself consistently.",
+    author: "Brian Tracy",
   },
   {
-    "quote": "Time rewards those who invest it wisely instead of spending it carelessly.",
-    "author": "Jim Rohn"
+    quote:
+      "Time rewards those who invest it wisely instead of spending it carelessly.",
+    author: "Jim Rohn",
   },
   {
-    "quote": "Your greatest competition is the person you were yesterday, not anyone else.",
-    "author": "Unknown"
+    quote:
+      "Your greatest competition is the person you were yesterday, not anyone else.",
+    author: "Unknown",
   },
   {
-    "quote": "Small improvements practiced every day eventually create results that seem impossible.",
-    "author": "James Clear"
+    quote:
+      "Small improvements practiced every day eventually create results that seem impossible.",
+    author: "James Clear",
   },
   {
-    "quote": "Courage is moving forward even when fear insists that you should stop.",
-    "author": "Nelson Mandela"
+    quote:
+      "Courage is moving forward even when fear insists that you should stop.",
+    author: "Nelson Mandela",
   },
   {
-    "quote": "The quality of your life reflects the quality of your daily decisions.",
-    "author": "Stephen R. Covey"
+    quote:
+      "The quality of your life reflects the quality of your daily decisions.",
+    author: "Stephen R. Covey",
   },
   {
-    "quote": "Consistency will carry you farther than motivation ever promises to take you.",
-    "author": "Zig Ziglar"
-  }
-]
+    quote:
+      "Consistency will carry you farther than motivation ever promises to take you.",
+    author: "Zig Ziglar",
+  },
+];
 
-const quoteText = document.querySelector("#quote-text")
-const quoteWritter = document.querySelector("#quote-writter")
+const quoteText = document.querySelector("#quote-text");
+const quoteWritter = document.querySelector("#quote-writter");
 
-
-setInterval(()=>{
-    let random = Math.floor(Math.random()*15)
-    quoteText.innerHTML = allMotQuote[random].quote;
-    quoteWritter.innerHTML = `- ${allMotQuote[random].author}`;
-}, 10000)
+setInterval(() => {
+  let random = Math.floor(Math.random() * 15);
+  quoteText.innerHTML = allMotQuote[random].quote;
+  quoteWritter.innerHTML = `- ${allMotQuote[random].author}`;
+}, 10000);
 
 //POMODORO TIMER
 
-const pomoOption1 = document.querySelector(".pomo-option1")
-const pomoOption2 = document.querySelector(".pomo-option2")
-const pomoTime = document.querySelector("#pomo-time")
-const pomoContext = document.querySelector("#pomo-context")
-const pomoStartButton = document.querySelector("#pomo-start")
-let timeLeft = 20
+const pomoOption1 = document.querySelector(".pomo-option1");
+const pomoOption2 = document.querySelector(".pomo-option2");
+const pomoTime = document.querySelector("#pomo-time");
+const pomoContext = document.querySelector("#pomo-context");
+const pomoStartButton = document.querySelector("#pomo-start");
+const pomoFunctions = document.querySelector(".pomo-funcs")
+
+let timeLeft = 2700;
 let interval;
 
-pomoStartButton.addEventListener('click', ()=>{
-  pomoStartButton.disabled = true
-  const updateTimer = ()=>{
-  const mins = Math.floor(timeLeft/60)
-  const seconds = timeLeft%60;
-  pomoTime.innerHTML = `${mins.toString().padStart(2, "0")}: ${seconds.toString().padStart(2,"0")}`
-  
-}
-  interval = setInterval(()=>{
-  timeLeft--;
-  updateTimer();
+pomoStartButton.addEventListener("click", () => {
+  pomoFunctions.innerHTML
+  const updateTimer = () => {
+    const mins = Math.floor(timeLeft / 60);
+    const seconds = timeLeft % 60;
+    pomoTime.innerHTML = `${mins.toString().padStart(2, "0")}: ${seconds.toString().padStart(2, "0")}`;
+  };
 
-  if(timeLeft === 0){
-    clearInterval(interval)
-    timeLeft = 2700;
-    alert("Time's Up!")
+  interval = setInterval(() => {
+    if (timeLeft === 0) {
+      clearInterval(interval);
+      timeLeft = 2700;
+      alert("Time's Up!");
+      updateTimer();
+    }
+    timeLeft--;
     updateTimer();
-  }
-
-
-  }, 1000)
-
-
-})
-
+  }, 1000);
+});
